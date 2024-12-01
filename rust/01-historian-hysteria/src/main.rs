a66f7f7b0ec8e376eb558269ddb27a2cf7bcaf9e
Stephan Göppentin <dedda102@gmail.com>
2024-12-01 13:00:44 +0100

    extract input parsing

--- rust/01-historian-hysteria/src/main.rs
+++ rust/01-historian-hysteria/src/main.rs
@@ -7,30 +7,30 @@ fn main() {
 }
 
 fn part_1(input: &str) -> usize {
-    let (mut left, mut right): (Vec<_>, Vec<_>)= input.lines().map(|line| {
-        let mut parts = line.split(' ').filter(|part| !part.is_empty());
-        let left = parts.next().unwrap().parse::<usize>().unwrap();
-        let right = parts.next().unwrap().parse::<usize>().unwrap();
-        return (left, right);
-    }).unzip();
+    let (mut left, mut right) = parse_input(input);
     left.sort();
     right.sort();
     left.iter().zip(right.iter()).map(|(a, b)| a.abs_diff(*b)).sum()
 }
 
 fn part_2(input: &str) -> usize {
-    let (left, right): (Vec<_>, Vec<_>)= input.lines().map(|line| {
-        let mut parts = line.split(' ').filter(|part| !part.is_empty());
-        let left = parts.next().unwrap().parse::<usize>().unwrap();
-        let right = parts.next().unwrap().parse::<usize>().unwrap();
-        return (left, right);
-    }).unzip();
+    let (left, right) = parse_input(input);
     left.iter()
         .map(|id|
             id * right.iter()
                 .filter(|rid| **rid == *id)
                 .count())
         .sum()
+}
+
+fn parse_input(input: &str) -> (Vec<usize>, Vec<usize>) {
+    let (left, right): (Vec<_>, Vec<_>) = input.lines().map(|line| {
+        let mut parts = line.split(' ').filter(|part| !part.is_empty());
+        let left = parts.next().unwrap().parse::<usize>().unwrap();
+        let right = parts.next().unwrap().parse::<usize>().unwrap();
+        return (left, right);
+    }).unzip();
+    (left, right)
 }
 
 #[cfg(test)]
