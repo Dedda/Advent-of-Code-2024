2284b0aaff28c1d5c17ffe4533657cf6fcc2503e
Stephan Göppentin <dedda102@gmail.com>
2024-12-01 20:17:21 +0100

    Use fancy Linq syntax

--- c-sharp/01 Historian Hysteria/01 Historian Hysteria/Program.cs
+++ c-sharp/01 Historian Hysteria/01 Historian Hysteria/Program.cs
@@ -3,34 +3,32 @@
 Console.WriteLine("Hello, World!");
 
 var input = File.ReadAllText("input.txt");
-var tuples = input
-    .Split('\n')
-    .ToList()
-    .Where(x => !string.IsNullOrWhiteSpace(x))
-    .Select(line => line
-        .Split(" ")
-        .Where(item => !string.IsNullOrWhiteSpace(item))
-        .Select(int.Parse)
-        .ToList())
-    .Select(items => (items[0], items[1]))
-    .ToList();
+var tuples = (from line in input.Split('\n') 
+        where !string.IsNullOrWhiteSpace(line)
+        select ToIntTuple(line)).ToList();
 
 Console.WriteLine($"Part 1: {Part1(tuples)}");
 Console.WriteLine($"Part 2: {Part2(tuples)}");
 return;
 
+(int, int) ToIntTuple(string line)
+{
+    var ints = (from part in line.Split(' ')
+        where !string.IsNullOrWhiteSpace(part)
+        select int.Parse(part)).ToList();
+    return (ints[0], ints[1]);
+}
+
 int Part1(List<(int, int)> items)
 {
-    var left = items.Select(tuple => tuple.Item1).ToList();
-    var right = items.Select(tuple => tuple.Item2).ToList();
-    left.Sort();
-    right.Sort();
+    var left = from tuple in items orderby tuple.Item1 select tuple.Item1;
+    var right = from tuple in items orderby tuple.Item2 select tuple.Item2;
     return left.Zip(right).Select(row => Math.Abs(row.First - row.Second)).Sum();
 }
 
 int Part2(List<(int, int)> items)
 {
-    var left = items.Select(tuple => tuple.Item1).ToList();
-    var right = items.Select(tuple => tuple.Item2).ToList();
+    var left = from tuple in items select tuple.Item1;
+    var right = from tuple in items select tuple.Item2;
     return left.Select(l => l * right.Count(r => r == l)).Sum();
 }
